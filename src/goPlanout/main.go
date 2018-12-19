fe536763f9cb5262c5f3fc7646ee602642f87ce4
wernzio <wernzio@gmail.com>
2018-12-18 23:51:49 -0500

    Allow execute to be used with waitgroups

--- src/goPlanout/main.go
+++ src/goPlanout/main.go
@@ -4,18 +4,16 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"strconv"
+	"sync"
 )
-
-type planoutParams struct {
-	Choice     string
-	Percentage string
-}
 
 type Experiment struct {
 	Key         string
 	PID         string
 	Choices     []interface{}
 	Percentages []float64
+	sum         float64
+	cweights    []float64
 }
 
 func hash(in string) uint64 {
@@ -30,18 +28,14 @@ func hash(in string) uint64 {
 	return z
 }
 
-func (exp *Experiment) getUniform(min, max float64) float64 {
+func (exp *Experiment) getUniform(PID string, min, max float64) float64 {
 	scale, _ := strconv.ParseUint("FFFFFFFFFFFFFFF", 16, 64)
-	h := exp.getHash()
+	h := hash(exp.Key + "." + PID)
 	shift := float64(h) / float64(scale)
 	return min + shift*(max-min)
 }
 
-func (exp *Experiment) getHash() uint64 {
-	return hash(exp.Key + "." + exp.PID)
-}
-
-func (exp *Experiment) getCummulativeWeights() (float64, []float64) {
+func (exp *Experiment) getCummulativeWeights() {
 	nweights := len(exp.Percentages)
 	cweights := make([]float64, nweights)
 	sum := 0.0
@@ -49,15 +43,19 @@ func (exp *Experiment) getCummulativeWeights() (float64, []float64) {
 		sum = sum + exp.Percentages[i]
 		cweights[i] = sum
 	}
-	return sum, cweights
+	exp.sum = sum
+	exp.cweights = cweights
 }
 
-func (exp *Experiment) Execute() interface{} {
-	sum, cweights := exp.getCummulativeWeights()
-	stop_val := exp.getUniform(0.0, sum)
-	fmt.Println(stop_val)
-	for i := range cweights {
-		if stop_val <= cweights[i] {
+func (exp *Experiment) Init() {
+	exp.getCummulativeWeights()
+}
+
+func (exp *Experiment) Execute(PID string, wg *sync.WaitGroup) interface{} {
+	defer wg.Done()
+	stop_val := exp.getUniform(PID, 0.0, exp.sum)
+	for i := range exp.cweights {
+		if stop_val <= exp.cweights[i] {
 			return exp.Choices[i]
 		}
 	}
--- src/main.go
+++ src/main.go
@@ -3,9 +3,21 @@ package main
 import (
 	planout "./goPlanout"
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	nexExp := planout.Experiment{Key: "blah", PID: "blah", Choices: []interface{}{true, false}, Percentages: []float64{0.5, 0.5}}
-	fmt.Println(nexExp.Execute())
+	start := time.Now()
+	nexExp := planout.Experiment{Key: "blah", Choices: []interface{}{true, false}, Percentages: []float64{0.5, 0.5}}
+	nexExp.Init()
+	var wg sync.WaitGroup
+	i := 0
+	for i < 1000000 {
+		wg.Add(1)
+		go nexExp.Execute("blah", &wg)
+		i++
+	}
+	wg.Wait()
+	fmt.Println(time.Since(start))
 }
